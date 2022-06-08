edb0a7924b3c264daa83185f88bfb0d9f7275d11
vihaansingla56 <vihaan1405.be20@chitkara.edu.in>
2022-06-08 23:59:26 +0530

    some files changes

--- connections/db.js
+++ connections/db.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  // host: "vihaan.cltt7kwbidtr.us-east-1.rds.amazonaws.com",
-  // user: "vihaan90",
-  // password: "Vihaan9092",
-  // database:"chatui",
-  // charset : 'utf8mb4'
-  host: "localhost",
-  user: "root",
-  password: "",
-  database:"chat-system",
+  host: "vihaan.cltt7kwbidtr.us-east-1.rds.amazonaws.com",
+  user: "vihaan90",
+  password: "Vihaan9092",
+  database:"chatui",
   charset : 'utf8mb4'
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database:"chat-system",
+  // charset : 'utf8mb4'
 
 });
 
