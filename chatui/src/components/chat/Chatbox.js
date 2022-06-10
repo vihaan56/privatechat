9b49cb2bd841bbfdd1f2fd2607821c39e68b23f2
vihaansingla56 <vihaan1405.be20@chitkara.edu.in>
2022-06-10 11:30:09 +0530

    some files changes

--- chatui/src/components/chat/Chatbox.js
+++ chatui/src/components/chat/Chatbox.js
@@ -1,4 +1,4 @@
-import './styles/chatbox.css';
+import "./styles/chatbox.css";
 import React, { useEffect, useState, useRef } from "react";
 import {
   BrowserRouter as Router,
@@ -21,7 +21,7 @@ socket = io(host);
 
 const Chatbox = () => {
   const [messages, setmessages] = useState([]);
-  const [typemessage,settypemessage] = useState("")
+  const [typemessage, settypemessage] = useState("");
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [selectchat, setselectchat] = useState(true);
@@ -35,7 +35,7 @@ const Chatbox = () => {
   }
 
   const getcompanies = async () => {
-    setLoading(true)
+    setLoading(true);
     const response = await fetch(`${host}/api/v1/routes/fetchchat`, {
       method: "POST",
       origin: true,
@@ -87,35 +87,12 @@ const Chatbox = () => {
   if (flag) {
     return <Navigate to="/login"></Navigate>;
   }
-  //  const sendmessage = async (e)=>{
-  //      if(typemessage === "") return;
-  //      var message = typemessage;
-  //     //  settypemessage("")
-  //      const response = await fetch(`${host}/api/v1/routes/sendmessage`, {
-  //       method: "POST",
-  //       origin: true,
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-
-  //       body: JSON.stringify({ user_id: userid, r_id: id, message: message }),
-  //     });
-
-  //     const json = await response.json();
-  //     // e.target.value = "";
-
-  //     setmessages((messages) => [...messages, json[0]]);
-  //     var data = { json, id };
-  //     socket.emit("new-message", data);
-  //  }
-  const sendmessage = async()=>{
-    var message =  typemessage;
-    var txtarea = document.getElementById("textarea");
-    txtarea.value="";
-    settypemessage("")
+  const sendmessage = async (e) => {
+    document.getElementById("textarea").focus();
+    e.preventDefault();
+    var message = e.target[0].value;
     if(message === "") return;
-
-    
+    e.target[0].value = "";
     const response = await fetch(`${host}/api/v1/routes/sendmessage`, {
       method: "POST",
       origin: true,
@@ -127,42 +104,80 @@ const Chatbox = () => {
     });
 
     const json = await response.json();
+    e.target.value = "";
 
     setmessages((messages) => [...messages, json[0]]);
     var data = { json, id };
     socket.emit("new-message", data);
-    txtarea.focus();
-  }
-  const handleChange = (e)=>{
-       settypemessage(e.target.value);
+  };
+  // const sendmessage = async()=>{
+  //   var message =  typemessage;
+  //   var txtarea = document.getElementById("textarea");
+  //   txtarea.value="";
+  //   settypemessage("")
+  //   if(message === "") return;
 
-       if (!typing) {
-        setTyping(true);
-        socket.emit("typing", { id, userid });
-      } else {
-        let lastTypingTime = new Date().getTime();
-        var timerLength = 2000;
-        setTimeout(() => {
-          var timeNow = new Date().getTime();
-          var timeDiff = timeNow - lastTypingTime;
-          if (timeDiff >= timerLength && typing) {
-            socket.emit("stop typing", { id, userid });
-            setTyping(false);
-          }
-        }, timerLength);
-      }
-  }
+  //   const response = await fetch(`${host}/api/v1/routes/sendmessage`, {
+  //     method: "POST",
+  //     origin: true,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+
+  //     body: JSON.stringify({ user_id: userid, r_id: id, message: message }),
+  //   });
+
+  //   const json = await response.json();
+
+  //   setmessages((messages) => [...messages, json[0]]);
+  //   var data = { json, id };
+  //   socket.emit("new-message", data);
+  //   txtarea.focus();
+  // }
+  // const handleChange = (e) => {
+  //   settypemessage(e.target.value);
+
+  //   if (!typing) {
+  //     setTyping(true);
+  //     socket.emit("typing", { id, userid });
+  //   } else {
+  //     let lastTypingTime = new Date().getTime();
+  //     var timerLength = 2000;
+  //     setTimeout(() => {
+  //       var timeNow = new Date().getTime();
+  //       var timeDiff = timeNow - lastTypingTime;
+  //       if (timeDiff >= timerLength && typing) {
+  //         socket.emit("stop typing", { id, userid });
+  //         setTyping(false);
+  //       }
+  //     }, timerLength);
+  //   }
+  // };
+  
   const _handleKeyDown = async (e) => {
-
+    if (!typing) {
+      setTyping(true);
+      socket.emit("typing", { id, userid });
+    } else {
+      let lastTypingTime = new Date().getTime();
+      var timerLength = 2000;
+      setTimeout(() => {
+        var timeNow = new Date().getTime();
+        var timeDiff = timeNow - lastTypingTime;
+        if (timeDiff >= timerLength && typing) {
+          socket.emit("stop typing", { id, userid });
+          setTyping(false);
+        }
+      }, timerLength);
+    }
     if (e.key === "Enter") {
-      settypemessage("")
       socket.emit("stop typing", { id, userid });
       var message = e.target.value;
       message = message.trim();
-      if(message === ""){
+      if (message === "") {
         return;
       }
-        
+
       e.target.value = "";
 
       const response = await fetch(`${host}/api/v1/routes/sendmessage`, {
@@ -188,65 +203,55 @@ const Chatbox = () => {
     <div>
       <div className="chatbox-container">
         <div className="full-container">
-          <div  style={{display:"none"}} className="main-1">
+          <div style={{ display: "none" }} className="main-1">
             <div className="main1-chat_top_container">Vihaan Singla</div>
             <div className="main1-usertopcontainer">
-              <div className="main-displayusers">
-
-              
-                  
-
-              
-        
-              </div>
+              <div className="main-displayusers"></div>
             </div>
           </div>
           <div className="main-2">
             <div className="chatbox">
               <div className="chat_top_container">Chat Box</div>
               <div id="chat_contain" className="chat_container">
-
-                   {/* {
+                {/* {
                      loading?"Loading....":""
                    } */}
-                  <ScrollableChat  messages={messages} id={id}></ScrollableChat>
+                <ScrollableChat messages={messages} id={id}></ScrollableChat>
 
-                  {/* <div className="typing">Typing...</div> */}
-
-    
+                {/* <div className="typing">Typing...</div> */}
               </div>
-
+           <form onSubmit={sendmessage}>
               <div className="textarea_container">
                 <div className="txt-mid">
                   <div className="textarea">
                     <textarea
                       id="textarea"
                       r_id={id}
-                      onChange={handleChange}
+                      autoFocus
                       onKeyDown={_handleKeyDown}
-                      autoCorrect="off"
                       autoCapitalize="off"
-                      spellCheck="false"
-                      value={typemessage}
                       className="textarea"
                       placeholder="Message..."
                       rows="1"
                     ></textarea>
                   </div>
                   <div className="send_button">
-                    <button type="submit" id="send" onClick={sendmessage} className="submit">
+                    <button
+                      type="submit"
+                      id="send"
+                     
+                      className="submit"
+                    >
                       Send
                     </button>
                   </div>
                 </div>
               </div>
+              </form>
             </div>
-
           </div>
         </div>
       </div>
-
-  
     </div>
   );
 };
